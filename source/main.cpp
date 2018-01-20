0c2251926da18ddf58cca83679f90f58e7ce9200
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-01-19 18:57:22 -0800

    [brief] Makes main useful.
    [detailed]
    - Main now actually polygonizes a sphere.

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,32 @@
 #include "athena/core/Log.hpp"
 #include "athena/Athena.hpp"
 
+#include "athena/fields/Sphere.hpp"
+#include "athena/tree/BlobTree.hpp"
+#include "athena/blob/Bsoid.hpp"
+
 int main()
 {
+    using athena::fields::ImplicitFieldPtr;
+    using athena::fields::Sphere;
+    using athena::tree::BlobTree;
+    using athena::blob::Bsoid;
+
     INFO_LOG_V("Welcome to Athena %s", ATHENA_VERSION_STRING);
+    INFO_LOG("Building model...");
+    ImplicitFieldPtr sphere = std::make_shared<Sphere>();
+    BlobTree tree;
+    tree.insertField(sphere);
+    tree.insertNodeTree({ {-1} });
+
+    INFO_LOG("Model done!");
+    INFO_LOG("Starting polygonization...");
+    Bsoid soid;
+    soid.setModel(tree);
+    soid.setName("sphere");
+    soid.polygonize(4, 8);
+    soid.saveCubicLattice();
+    INFO_LOG("Polygonization done!");
 
     return 0;
 }
