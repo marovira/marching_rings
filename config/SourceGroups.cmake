838f457683bf131bed27f7a4a92a788b36f9e1eb
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-01-22 13:53:33 -0800

    [brief] Updates CMake and gitignore.
    [detailed]

--- config/SourceGroups.cmake
+++ config/SourceGroups.cmake
@@ -6,10 +6,18 @@ source_group("include\\athena" FILES)
 source_group("include\\athena\\core" FILES ${ATHENA_INCLUDE_CORE_GROUP})
 source_group("include\\athena\\fields" FILES ${ATHENA_INCLUDE_FIELDS_GROUP})
 source_group("include\\athena\\tree" FILES ${ATHENA_INCLUDE_TREE_GROUP})
-source_group("include\\athena\\blob" FILES ${ATHENA_INCLUDE_BLOB_GROUP})
+source_group("include\\athena\\polygonizer" FILES 
+    ${ATHENA_INCLUDE_POLYGONIZER_GROUP})
 
 source_group("source" FILES ${ATHENA_SOURCE_TOP_GROUP})
 source_group("source\\athena" FILES)
 source_group("source\\athena\\core" FILES ${ATHENA_SOURCE_CORE_GROUP})
 source_group("source\\athena\\tree" FILES ${ATHENA_SOURCE_TREE_GROUP})
-source_group("source\\athena\\blob" FILES ${ATHENA_SOURCE_BLOB_GROUP})
+source_group("source\\athena\\polygonizer" FILES 
+    ${ATHENA_SOURCE_POLYGONIZER_GROUP})
+
+source_group("shader" FILES ${ATHENA_SHADER_TOP_GROUP})
+source_group("shader\\athena" FILES)
+source_group("shader\\athena\\global" FILES ${ATHENA_SHADER_GLOBAL_GROUP})
+source_group("shader\\athena\\visualizer" FILES
+    ${ATHENA_SHADER_VISUALIZER_GROUP})
