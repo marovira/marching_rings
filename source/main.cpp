721b7b86a54d9bfe02acb5c78d1498a4b4e0e75f
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-01-30 14:47:50 -0800

    [brief] Updates main.
    [detailed]
    - Adds a function to create a simple sphere model.
    - Updates the GUI version to use the new interfaces.

--- source/main.cpp
+++ source/main.cpp
@@ -5,12 +5,31 @@
 
 #include "athena/fields/Sphere.hpp"
 #include "athena/tree/BlobTree.hpp"
-//#include "athena/blob/Bsoid.hpp"
+#include "athena/polygonizer/Bsoid.hpp"
 
 #include <atlas/core/Log.hpp>
 #include <atlas/utils/Application.hpp>
 #include <atlas/utils/WindowSettings.hpp>
 #include <atlas/gl/ErrorCheck.hpp>
+
+athena::polygonizer::Bsoid makeSphere()
+{
+    // Polygonizer using.
+    using athena::fields::ImplicitFieldPtr;
+    using athena::fields::Sphere;
+    using athena::tree::BlobTree;
+    using athena::polygonizer::Bsoid;
+    using athena::polygonizer::SlicingAxes;
+
+    ImplicitFieldPtr sphere = std::make_shared<Sphere>();
+    BlobTree tree;
+    tree.insertField(sphere);
+    tree.insertNodeTree({ {-1} });
+
+    return Bsoid(tree, "sphere");
+}
+
+
 
 #if (ATHENA_USE_GUI)
 
@@ -22,10 +41,8 @@ int main()
     using atlas::utils::Application;
     using atlas::utils::ScenePointer;
 
-    // Polygonizer using.
-    using athena::fields::ImplicitFieldPtr;
-    using athena::fields::Sphere;
-    using athena::tree::BlobTree;
+    std::vector<athena::polygonizer::Bsoid> models;
+    models.emplace_back(makeSphere());
 
     atlas::gl::setGLErrorSeverity(ATLAS_GL_ERROR_SEVERITY_HIGH |
         ATLAS_GL_ERROR_SEVERITY_MEDIUM);
@@ -38,7 +55,7 @@ int main()
 
     Application::getInstance().createWindow(settings);
     Application::getInstance().addScene(ScenePointer(
-        new athena::visualizer::ModelVisualizer()));
+        new athena::visualizer::ModelVisualizer(models)));
     Application::getInstance().runApplication();
 
     return 0;
