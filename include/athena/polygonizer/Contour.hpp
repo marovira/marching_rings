24ff95855343d5f8b5d1915726b04fcc2f290111
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-02-07 18:13:41 -0800

    [brief] Adds the contour class.
    [detailed]
    - This will hold the OpenGL representation of the Contour later on.

---
+++ include/athena/polygonizer/Contour.hpp
@@ -0,0 +1,23 @@
+#ifndef ATHENA_INCLUDE_ATHENA_POLYGONIZER_CONTOUR_HPP
+#define ATHENA_INCLUDE_ATHENA_POLYGONIZER_CONTOUR_HPP
+
+#pragma once
+
+#include "Polygonizer.hpp"
+
+#include <atlas/math/Math.hpp>
+#include <vector>
+
+namespace athena
+{
+    namespace polygonizer
+    {
+        struct Contour
+        {
+            Contour() = default;
+
+        };
+    }
+}
+
+#endif
