e68ac0704d71d0eaef9a8c4e0fa44db0c14d72f4
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-02-13 15:51:20 -0800

    [brief] Fixes for Sphere.
    [detailed]
    - Fixes the way the bounding box is computed when the centre is not the
      origin.
    - Fixes the way the seeds are computed. The previous version was wrong
      and only worked for the trivial case.

--- include/athena/fields/Sphere.hpp
+++ include/athena/fields/Sphere.hpp
@@ -33,15 +33,32 @@ namespace athena
             std::vector<atlas::math::Point> getSeeds(
                 atlas::math::Normal const& u) const override
             {
+                // First project the centre onto the plane.
+                auto v = mCentre - u;
+                auto d = glm::proj(v, glm::normalize(u));
+                auto proj = mCentre - d;
+
                 float radius = mRadius;
-                float l = glm::length(u - mCentre);
+                float l = glm::length(proj - mCentre);
                 if (l > radius)
                 {
                     return {};
                 }
 
-                float rp = (radius * radius) - glm::length(u - mCentre);
-                return { u + rp };
+                float rp = 
+                    glm::sqrt((radius * radius) - glm::length2(proj - mCentre));
+                auto seed = proj;
+                for (int i = 0; i < 3; ++i)
+                {
+                    if (u[i] == 0.0f)
+                    {
+                        seed[i] += rp;
+                        break;
+                    }
+                }
+
+                assert(eval(seed) == 0.0f);
+                return { seed };
             }
 
         private:
@@ -55,7 +72,10 @@ namespace athena
                 using atlas::utils::BBox;
                 using atlas::math::Point;
 
-                return BBox(Point(-mRadius), Point(mRadius));
+                Point min = mCentre - mRadius;
+                Point max = mCentre + mRadius;
+
+                return BBox(min, max);
             }
 
             float mRadius;
