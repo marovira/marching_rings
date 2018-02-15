aafde8d3f025a1ac9bbb7e6f6a5c7d8217b1901d
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-02-15 14:08:51 -0800

    [brief] Removes the ability to change the filter fall off.
    [detailed]
    - The way the polygonizer is written relies heavily on knowing exactly
      what the primitives are doing. Currently supports only global, but
      will add compact support later on.

--- include/athena/fields/ImplicitField.hpp
+++ include/athena/fields/ImplicitField.hpp
@@ -18,8 +18,7 @@ namespace athena
         class ImplicitField
         {
         public:
-            ImplicitField() :
-                mFilter(global)
+            ImplicitField()
             { }
 
             virtual ~ImplicitField() = default;
@@ -31,14 +30,9 @@ namespace athena
                 return b;
             }
 
-            void setFilter(FilterFn const& filter)
-            {
-                mFilter = filter;
-            }
-
             float eval(atlas::math::Point const& p) const
             {
-                return mFilter(sdf(p));
+                return global(sdf(p));
             }
 
             virtual atlas::math::Normal grad(
@@ -49,10 +43,6 @@ namespace athena
         protected:
             virtual float sdf(atlas::math::Point const& p) const = 0;
             virtual atlas::utils::BBox box() const = 0;
-
-        private:
-            FilterFn mFilter;
-
         };
     }
 }
