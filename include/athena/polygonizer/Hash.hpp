1e2454a5bd3b7bbcb976384159c704b8e0ca7df9
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-03-15 16:58:43 -0700

    [brief] Adds a way to undo the hash function in debug mode.
    [detailed]
    - This helps to see what where the original coordinates that were used
      to generate that particular hash.

--- include/athena/polygonizer/Hash.hpp
+++ include/athena/polygonizer/Hash.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <atlas/core/Macros.hpp>
+
 #include <cinttypes>
 #include <limits>
 
@@ -23,6 +25,28 @@ namespace athena
 
         using BsoidHash32 = BsoidHash<std::uint32_t>;
         using BsoidHash64 = BsoidHash<std::uint64_t>;
+
+#ifdef ATLAS_DEBUG
+#include <atlas/math/Math.hpp>
+#include <tuple>
+
+        glm::u32vec2 reverseHash32(std::uint32_t hash)
+        {
+            std::uint32_t mask = 0xFFFF;
+            std::uint32_t y = hash & mask;
+            std::uint32_t x = (hash & (mask << 16)) >> 16;
+            return { x, y };
+        }
+
+        std::tuple<glm::u32vec2, glm::u32vec2> reverseHash64(std::uint64_t hash)
+        {
+            std::uint64_t mask = 0xFFFFFFFF;
+            std::uint32_t u = hash & mask;
+            std::uint32_t v = (hash & (mask << 32)) >> 32;
+
+            return { reverseHash32(u), reverseHash32(v) };
+        }
+#endif
     }
 }
 
