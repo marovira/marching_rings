6c6eb4b699f29ccb34e5213e32b5c551f4acb200
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-03-15 17:39:21 -0700

    [brief] Updates main to use new model functions.
    [detailed]

--- source/main.cpp
+++ source/main.cpp
@@ -1,72 +1,27 @@
 #include "athena/Athena.hpp"
 
+
 #include "athena/visualizer/ModelView.hpp"
 #include "athena/visualizer/ModelVisualizer.hpp"
-
-#include "athena/fields/Sphere.hpp"
-#include "athena/operators/Blend.hpp"
-#include "athena/tree/BlobTree.hpp"
-#include "athena/polygonizer/Bsoid.hpp"
+#include "athena/models/Models.hpp"
 
 #include <atlas/core/Log.hpp>
 #include <atlas/utils/Application.hpp>
 #include <atlas/utils/WindowSettings.hpp>
 #include <atlas/gl/ErrorCheck.hpp>
 
-athena::polygonizer::Bsoid makeSphere()
-{
-    // Polygonizer using.
-    using athena::fields::ImplicitFieldPtr;
-    using athena::fields::Sphere;
-    using athena::tree::BlobTree;
-    using athena::polygonizer::Bsoid;
-    using athena::polygonizer::SlicingAxes;
-
-    ImplicitFieldPtr sphere = std::make_shared<Sphere>();
-    BlobTree tree;
-    tree.insertField(sphere);
-    tree.insertNodeTree({ { -1 } });
-    tree.insertFieldTree(sphere);
-
-    Bsoid soid(tree, "sphere");
-    soid.setSlicingAxis(SlicingAxes::YAxis);
-    soid.setNumCrossSections(16);
-    soid.makeCrossSections(8, 4);
-    return soid;
-}
-
-athena::polygonizer::Bsoid makePeanut()
-{
-    // Polygonizer using.
-    using atlas::math::Point;
-    using athena::fields::ImplicitFieldPtr;
-    using athena::operators::ImplicitOperatorPtr;
-    using athena::fields::Sphere;
-    using athena::operators::Blend;
-    using athena::tree::BlobTree;
-    using athena::polygonizer::Bsoid;
-    using athena::polygonizer::SlicingAxes;
-
-    ImplicitFieldPtr sphere1 = std::make_shared<Sphere>(1.0f, Point(1.0f, 0, 0));
-    ImplicitFieldPtr sphere2 = std::make_shared<Sphere>(1.0f, Point(-1.0f, 0, 0));
-    ImplicitOperatorPtr blend = std::make_shared<Blend>();
-    blend->insertFields({ sphere1, sphere2 });
-
-    BlobTree tree;
-    tree.insertFields({ sphere1, sphere2, blend });
-    tree.insertNodeTree({ { -1 }, { -1 }, { 0, 1 } });
-    tree.insertFieldTree(blend);
-
-    Bsoid soid(tree, "peanut");
-    soid.setSlicingAxis(SlicingAxes::XAxis);
-    soid.setNumCrossSections(32);
-    soid.makeCrossSections(16, 4);
-    return soid;
-}
-
-
 
 #if (ATHENA_USE_GUI)
+
+std::vector<athena::models::ModelFn> getModels()
+{
+    using namespace athena::models;
+    std::vector<athena::models::ModelFn> result;
+    result.push_back(makeSphere);
+    result.push_back(makePeanut);
+
+    return result;
+}
 
 int main()
 {
@@ -77,8 +32,11 @@ int main()
     using atlas::utils::ScenePointer;
 
     std::vector<athena::polygonizer::Bsoid> models;
-    models.emplace_back(makeSphere());
-    //models.emplace_back(makePeanut());
+    auto modelFns = getModels();
+    for (auto& modelFn : modelFns)
+    {
+        models.emplace_back(modelFn());
+    }
 
     atlas::gl::setGLErrorSeverity(ATLAS_GL_ERROR_SEVERITY_HIGH |
         ATLAS_GL_ERROR_SEVERITY_MEDIUM);
@@ -101,11 +59,6 @@ int main()
 
 int main()
 {
-    using athena::fields::ImplicitFieldPtr;
-    using athena::fields::Sphere;
-    using athena::tree::BlobTree;
-    //using athena::blob::Bsoid;
-
     INFO_LOG_V("Welcome to Athena %s", ATHENA_VERSION_STRING);
     INFO_LOG("Building model...");
 
