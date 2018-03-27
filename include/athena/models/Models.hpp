7c7334de742d2341dcde5b448ae1f41c6a4af387
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-03-27 13:56:35 -0700

    [brief] Adds the make functions for MC models.
    [detailed]

--- include/athena/models/Models.hpp
+++ include/athena/models/Models.hpp
@@ -4,23 +4,35 @@
 #pragma once
 
 #include "athena/polygonizer/Bsoid.hpp"
+#include "athena/polygonizer/MarchingCubes.hpp"
 
 #include <functional>
 
-#define MAKE_FUNCTION(name) \
+#define MAKE_SOID_FUNCTION(name) \
 athena::polygonizer::Bsoid make##name()
+
+#define MAKE_MC_FUNCTION(name) \
+athena::polygonizer::MarchingCubes makeMC##name()
+
 
 namespace athena
 {
     namespace models
     {
         using ModelFn = std::function<athena::polygonizer::Bsoid()>;
+        using MCModelFn = std::function<athena::polygonizer::MarchingCubes()>;
 
-        MAKE_FUNCTION(Sphere);
-        MAKE_FUNCTION(Peanut);
-        MAKE_FUNCTION(Cylinder);
-        MAKE_FUNCTION(Cone);
-        MAKE_FUNCTION(Torus);
+        MAKE_SOID_FUNCTION(Sphere);
+        MAKE_SOID_FUNCTION(Peanut);
+        MAKE_SOID_FUNCTION(Cylinder);
+        MAKE_SOID_FUNCTION(Cone);
+        MAKE_SOID_FUNCTION(Torus);
+
+        MAKE_MC_FUNCTION(Sphere);
+        MAKE_MC_FUNCTION(Peanut);
+        MAKE_MC_FUNCTION(Cylinder);
+        MAKE_MC_FUNCTION(Cone);
+        MAKE_MC_FUNCTION(Torus);
     }
 }
 
--- source/athena/models/Models.cpp
+++ source/athena/models/Models.cpp
@@ -21,15 +21,22 @@ namespace athena
         using operators::ImplicitOperatorPtr;
         using tree::BlobTree;
         using polygonizer::Bsoid;
+        using polygonizer::MarchingCubes;
         using polygonizer::SlicingAxes;
 
         using Resolution = std::tuple<std::size_t, std::size_t, std::size_t>;
+        using MCResolution = glm::u32vec3;
 
         constexpr Resolution lowResolution = { 16, 8, 4 };
         constexpr Resolution midResolution = { 32, 16, 8 };
         constexpr Resolution highResolution = { 64, 32, 8 };
 
-        constexpr Resolution currentResolution = lowResolution;
+        static const MCResolution lowResolutionMC = { 16, 8, 8 };
+        static const MCResolution midResolutionMC = { 32, 16, 16 };
+        static const MCResolution highResolutionMC = { 64, 32, 32 };
+
+        constexpr Resolution currentResolution = midResolution;
+        static const MCResolution currentResolutionMC = midResolutionMC;
 
         polygonizer::Bsoid makeSphere()
         {
@@ -47,6 +54,21 @@ namespace athena
             soid.makeCrossSections(std::get<1>(currentResolution),
                 std::get<2>(currentResolution));
             return soid;
+        }
+
+        polygonizer::MarchingCubes makeMCSphere()
+        {
+            using fields::Sphere;
+
+            ImplicitFieldPtr sphere = std::make_shared<Sphere>();
+            BlobTree tree;
+            tree.insertField(sphere);
+            tree.insertNodeTree({ { -1 } });
+            tree.insertFieldTree(sphere);
+
+            MarchingCubes mc(tree, "sphere");
+            mc.setResolution(currentResolutionMC.yxz());
+            return mc;
         }
 
         polygonizer::Bsoid makePeanut()
@@ -68,12 +90,35 @@ namespace athena
             tree.insertFieldTree(blend);
 
             Bsoid soid(tree, "peanut");
-            soid.setSlicingAxis(SlicingAxes::YAxis);
+            soid.setSlicingAxis(SlicingAxes::XAxis);
             soid.setNumCrossSections(std::get<0>(currentResolution));
             soid.makeCrossSections(std::get<1>(currentResolution),
                 std::get<2>(currentResolution));
             return soid;
+        }
 
+        polygonizer::MarchingCubes makeMCPeanut()
+        {
+            using atlas::math::Point;
+            using fields::Sphere;
+            using operators::Blend;
+
+            ImplicitFieldPtr sphere1 =
+                std::make_shared<Sphere>(1.0f, Point(1.0f, 0, 0));
+            ImplicitFieldPtr sphere2 =
+                std::make_shared<Sphere>(1.0f, Point(-1.0f, 0, 0));
+            ImplicitOperatorPtr blend = std::make_shared<Blend>();
+            blend->insertFields({ sphere1, sphere2 });
+
+            BlobTree tree;
+            tree.insertFields({ sphere1, sphere2, blend });
+            tree.insertNodeTree({ { -1 }, { -1 }, { 0, 1 } });
+            tree.insertFieldTree(blend);
+
+            MarchingCubes mc(tree, "peanut");
+            mc.setResolution(currentResolutionMC.xyz());
+
+            return mc;
         }
 
         polygonizer::Bsoid makeCylinder()
@@ -93,6 +138,22 @@ namespace athena
             soid.makeCrossSections(std::get<1>(currentResolution),
                 std::get<2>(currentResolution));
             return soid;
+        }
+        
+        polygonizer::MarchingCubes makeMCCylinder()
+        {
+            using atlas::math::Point;
+            using fields::Cylinder;
+
+            ImplicitFieldPtr cylinder = std::make_shared<Cylinder>();
+            BlobTree tree;
+            tree.insertField(cylinder);
+            tree.insertNodeTree({ { -1 } });
+            tree.insertFieldTree(cylinder);
+
+            MarchingCubes mc(tree, "cylinder");
+            mc.setResolution(currentResolutionMC.zyx());
+            return mc;
         }
 
         polygonizer::Bsoid makeCone()
@@ -114,6 +175,22 @@ namespace athena
             return soid;
         }
 
+        polygonizer::MarchingCubes makeMCCone()
+        {
+            using atlas::math::Point;
+            using fields::Cone;
+
+            ImplicitFieldPtr cone = std::make_shared<Cone>();
+            BlobTree tree;
+            tree.insertField(cone);
+            tree.insertNodeTree({ { -1 } });
+            tree.insertFieldTree(cone);
+
+            MarchingCubes mc(tree, "cone");
+            mc.setResolution(currentResolutionMC.zyx());
+            return mc;
+        }
+
         polygonizer::Bsoid makeTorus()
         {
             using atlas::math::Point;
@@ -126,11 +203,27 @@ namespace athena
             tree.insertFieldTree(torus);
 
             Bsoid soid(tree, "torus");
-            soid.setSlicingAxis(SlicingAxes::ZAxis);
+            soid.setSlicingAxis(SlicingAxes::YAxis);
             soid.setNumCrossSections(std::get<0>(currentResolution));
             soid.makeCrossSections(std::get<1>(currentResolution),
                 std::get<2>(currentResolution));
             return soid;
+        }
+
+        polygonizer::MarchingCubes makeMCTorus()
+        {
+            using atlas::math::Point;
+            using fields::Torus;
+
+            ImplicitFieldPtr torus = std::make_shared<Torus>();
+            BlobTree tree;
+            tree.insertField(torus);
+            tree.insertNodeTree({ { -1 } });
+            tree.insertFieldTree(torus);
+
+            MarchingCubes mc(tree, "torus");
+            mc.setResolution(currentResolutionMC.yxz());
+            return mc;
         }
     }
 }
