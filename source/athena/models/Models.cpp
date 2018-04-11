4f710f735ed7048d8558c1a69545c0fbf6fa8f0c
Mauricio Andres Rovira Galvez <marovira@uvic.ca>
2018-04-10 17:34:09 -0700

    [brief] Moves the torus to be sliced along Y.
    [detailed]
    - Since we have a (mostly) working solution for same number of contours
      per slice, we now move forward to the branching problem proper.

--- source/athena/models/Models.cpp
+++ source/athena/models/Models.cpp
@@ -35,7 +35,7 @@ namespace athena
         static const MCResolution midResolutionMC = { 32, 16, 16 };
         static const MCResolution highResolutionMC = { 64, 32, 32 };
 
-        constexpr Resolution currentResolution = midResolution;
+        constexpr Resolution currentResolution = highResolution;
         static const MCResolution currentResolutionMC = midResolutionMC;
 
         polygonizer::Bsoid makeSphere()
@@ -203,7 +203,7 @@ namespace athena
             tree.insertFieldTree(torus);
 
             Bsoid soid(tree, "torus");
-            soid.setSlicingAxis(SlicingAxes::ZAxis);
+            soid.setSlicingAxis(SlicingAxes::YAxis);
             soid.setNumCrossSections(std::get<0>(currentResolution));
             soid.makeCrossSections(std::get<1>(currentResolution),
                 std::get<2>(currentResolution));
